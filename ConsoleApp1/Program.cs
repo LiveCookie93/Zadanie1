839cd54c017fbffbfb07b6e66628e0774817a940
vsproko1@gmail.com <vsproko1@gmail.com>
2021-10-26 22:44:49 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Zadanie1();
+        }
+        public static void Zadanie1()
+        {
+            Random rnd = new Random();
+            long[] a = new long[1000]; // размерность массива
+            long sum = 0;
+            for (long i = 0; i < a.Length; i++)
+            {
+                long r = rnd.Next(-10000, 10000); //значения массива
+                a[i] = r;
+            }
+            for (long i = 0; i < a.Length; i++)
+            {
+                sum = sum + a[i];
+                //Console.WriteLine(sum);
+            }
+            Console.WriteLine("Сумма чисел равна: {0} ", sum);
+            //Console.WriteLine(sum);
+        }
+    }
+}
